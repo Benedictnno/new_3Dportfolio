514657466b9e4f1342405596811117f6d5361bb7
Benedictnno <benedictnnaoma0@gmail.com>
2026-04-23 11:45:26 +0100

    fix: hero background

--- components/three/Canvas3D.tsx
+++ components/three/Canvas3D.tsx
@@ -1,11 +1,23 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Environment, BakeShadows } from '@react-three/drei'
 
 export function Canvas3D({ children }: { children: React.ReactNode }) {
+	const [eventSource, setEventSource] = useState<HTMLElement | undefined>()
+
+	useEffect(() => {
+		setEventSource(document.body)
+	}, [])
+
 	return (
-		<Canvas dpr={[1, 2]} shadows camera={{ position: [0, 1.2, 6], fov: 45 }}>
+		<Canvas 
+			eventSource={eventSource}
+			eventPrefix="client"
+			dpr={[1, 2]} 
+			shadows 
+			camera={{ position: [0, 1.2, 6], fov: 45 }}
+		>
 			<Suspense fallback={null}>
 				<Environment preset="city" blur={0.6} />
 				{children}
@@ -14,6 +26,3 @@ export function Canvas3D({ children }: { children: React.ReactNode }) {
 		</Canvas>
 	)
 }
-
-
-
